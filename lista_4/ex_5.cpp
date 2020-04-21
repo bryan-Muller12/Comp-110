a34c3561220bbaf0a91e77bdbdce9f171bd2532c
bryan-Muller12 <63310088+bryan-Muller12@users.noreply.github.com>
2020-04-21 18:23:51 +0000

    Create ex_5.cpp

---
+++ lista_4/ex_5.cpp
@@ -0,0 +1,14 @@
+//Exercício 5 
+
+#include <stdio.h>
+
+int main(void) {
+int m,s ;
+printf("Digite o minuto a ser avaliado\n");
+scanf("%d",&m);
+s=m*60;
+
+printf("Em %d minutos, contém %d segundos\n",m,s);
+
+return 0;
+}
