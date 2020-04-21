23e6555414bfed34d94479856c4443206b5aa18a
bryan-Muller12 <63310088+bryan-Muller12@users.noreply.github.com>
2020-04-21 18:32:20 +0000

    Create ex_12.cpp

---
+++ lista_4/ex_12.cpp
@@ -0,0 +1,25 @@
+//Execício 12
+
+#include <stdio.h>
+
+int main(void) {
+int x=1,y=20, z=1;
+
+while (x<= 20) 
+{
+printf("%d ", x); 
+x++; 
+} 
+printf("\n");
+while (y>=1) 
+{ 
+printf("%d ", y); 
+y--; 
+} 
+printf("\n");
+while (z<= 20) 
+{
+if (z%2 !=0) printf ("%d ",z);
+z++;
+}
+}
