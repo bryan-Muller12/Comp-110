74cc0134f29499eaff1dcf8ba365f227c3fca9b4
bryan-Muller12 <63310088+bryan-Muller12@users.noreply.github.com>
2020-04-21 18:26:52 +0000

    Create ex_9.cpp

---
+++ lista_4/ex_9.cpp
@@ -0,0 +1,18 @@
+//Execício 9
+
+
+#include <stdio.h>
+
+int main(void) {
+int x;
+for(x = 0; x <1; x++){
+
+printf("#######################\n");
+printf("#                     #\n");
+printf("#                     #\n");
+printf("#     SEJA BEM-VINDO  #\n");
+printf("#                     #\n");
+printf("#                     #\n");
+printf("#######################\n");
+}
+}
