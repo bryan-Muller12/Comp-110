648152d251d6cf1544d95b690bad37af1de04b8e
bryan-Muller12 <63310088+bryan-Muller12@users.noreply.github.com>
2020-04-21 18:32:49 +0000

    Create ex_13.cpp

---
+++ lista_4/ex_13.cpp
@@ -0,0 +1,14 @@
+//Exercício 13
+
+#include <stdio.h>
+
+int main(void) {
+int n1,n2, x;
+printf("digite dois numeros\n");
+scanf("%d%d",&n1,&n2);
+
+for (x=n1;x<=n2;x++){
+printf("%d ",x);
+}
+return 0;
+}
