d59a56944cc4f3ffac62d4c12ca9120f9454f730
bryan-Muller12 <63310088+bryan-Muller12@users.noreply.github.com>
2020-04-21 18:34:50 +0000

    Create ex_16.cpp

---
+++ lista_4/ex_16.cpp
@@ -0,0 +1,24 @@
+//Execícios 16
+
+#include <stdio.h>
+
+int main(void) {
+int n1,n2, x;
+printf("digite dois numeros\n");
+scanf("%d%d",&n1,&n2);
+for (x=n1;x<=n2;x++){
+printf("\n");
+printf("%d x 0 = %d\n",x,x*0);
+printf("%d x 1 = %d\n",x,x*1);
+printf("%d x 2 = %d\n",x,x*2);
+printf("%d x 3 = %d\n",x,x*3);
+printf("%d x 4 = %d\n",x,x*4);
+printf("%d x 5 = %d\n",x,x*5);
+printf("%d x 6 = %d\n",x,x*6);
+printf("%d x 7 = %d\n",x,x*7);
+printf("%d x 8 = %d\n",x,x*8);
+printf("%d x 9 = %d\n",x,x*9);
+printf("%d x 10 = %d\n",x,x*10);
+printf("\n");
+}
+return 0;
